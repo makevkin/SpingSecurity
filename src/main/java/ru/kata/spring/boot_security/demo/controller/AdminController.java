ae39532234c9a4f628fc302f6c9885b3271210d6
Юрий Макевкин <makevkin@mail.ru>
2024-02-19 21:56:14 +0400

    2 version

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -35,63 +35,6 @@ public class AdminController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("")
-    public String indexView(Model model, Principal principal) {
-        String username = principal.getName();
-        model.addAttribute("users", userService.findAll());
-        model.addAttribute("username", principal.getName());
-        model.addAttribute("user", userRepository.findByUsername(principal.getName()).get());
-        model.addAttribute("allRoles", roleRepository.findAll());
-        model.addAttribute("newUser", new User());
-        return "indexSpringMvc";
-    }
-
-    @GetMapping("/addUser")
-    public String addUserView(Model model, Principal principal) {
-        User user = new User();
-        String username = principal.getName();
-        model.addAttribute("user", user);
-        List<Role> roles = (List<Role>) roleRepository.findAll();
-        model.addAttribute("username", username);
-        model.addAttribute("allRoles", roles);
-        return "addUserField";
-    }
-
-    @PatchMapping("/addOrUpdate/{id}")
-    public String add(@PathVariable("id") Long id, @Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            List<Role> roles = (List<Role>) roleRepository.findAll();
-            model.addAttribute("allRoles", roles);
-            return "addUserField";
-        } else {
-            userService.update(id, user);
-            return "redirect:/admin";
-        }
-
-    }
-
-    @PostMapping("/addOrUpdate")
-    public String add(@Valid @ModelAttribute("user") User user, Model model) {
-        userService.save(user);
-        return "redirect:/admin";
-    }
-
-
-    @DeleteMapping("/removeUser/{id}")
-    public String deleteUser(@PathVariable("id") Long id) {
-        userService.delete(id);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/updateUser")
-    public String updateUser(@ModelAttribute("id") Long id, Model model) {
-        User user = userService.findById(id).get();
-        model.addAttribute("user", user);
-        List<Role> roles = (List<Role>) roleRepository.findAll();
-        model.addAttribute("allRoles", roles);
-        return "editPage";
-    }
-
-
+   
 
 }
